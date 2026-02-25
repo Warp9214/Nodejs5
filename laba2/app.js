7b4ccc5bc61e376a74f99d7017253ae0b5cf08f3
Sergey0645 <yarserg05@gmail.com>
2026-02-25 19:39:29 +0200

    Initialize Express server with team route

--- laba2/app.js
+++ laba2/app.js
@@ -1,1 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/team', (req, res) => {
+    const brigadeInfo = {
+        number: 5,
+        students: [
+            { name: 'Ярошенко Сергій', role: '' },
+        ],
+        repoLink: 'https://github.com/expressjs/express'
+    };
+    
+    res.render('team', { info: brigadeInfo });
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущено на http://localhost:${port}`);
+});
+
+
+
+
 
