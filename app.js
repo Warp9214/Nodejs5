ac696d43b7aa6fc067a3ca2f0de0256dc6e06d35
Warp9214 <kovamu0b@gmail.com>
2026-02-25 17:59:35 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/team', (req, res) => {
+    const brigadeInfo = {
+        number: 5,
+        students: [
+            { name: 'Сивенюк Ольга', role: '' },
+        ],
+        repoLink: 'https://github.com/expressjs/express'
+    };
+    
+    res.render('team', { info: brigadeInfo });
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущено на http://localhost:${port}`);
+});
