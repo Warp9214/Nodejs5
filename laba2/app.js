1343f4ea75e48dda9c79716d062766e9e444153b
Sergey0645 <yarserg05@gmail.com>
2026-02-26 19:00:41 +0200

    Add student 'Плисюк Ростислав' to brigadeInfo

--- laba2/app.js
+++ laba2/app.js
@@ -11,7 +11,7 @@ app.get('/team', (req, res) => {
         number: 5,
         students: [
             { name: 'Ярошенко Сергій', role: '' },
-            { name:
+            { name: 'Плисюк Ростислав', role: '' },
             { name: 'Ковальногов Олександр', role: '' },
             { name: 'Ковальов Михайло ', role: '' },
             { name: 'Сивенюк Ольга', role: '' },
@@ -25,4 +25,5 @@ app.get('/team', (req, res) => {
 app.listen(port, () => {
     console.log(`Сервер запущено на http://localhost:${port}`);
 });
+
 
